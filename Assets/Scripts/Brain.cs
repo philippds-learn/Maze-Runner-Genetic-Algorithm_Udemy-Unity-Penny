e7a0094ddbbe508748ca80dfb769467de7129f2c
Philipp Siedler <Philipp.Siedler@zaha-hadid.com>
2019-11-27 19:30:56 +0000

    init repo

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    int DNALength = 2;
+    public DNA dna;
+    public GameObject eyes;
+    public bool alive = true;
+    bool seeWall = true;
+
+    Vector3 startPosition;
+    public float distanceTravelled = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "dead")
+        {
+            this.distanceTravelled = 0;
+            this.alive = false;
+        }
+    }
+
+    public void Init()
+    {
+        // initialise DNA
+        // 0 forward
+        // 1 Angle turn
+        this.dna = new DNA(this.DNALength, 360);
+        this.startPosition = this.transform.position;
+    }
+
+    private void Update()
+    {
+        if (!this.alive) return;
+
+        Vector3 pos = this.eyes.transform.position;
+        Vector3 forward = this.eyes.transform.forward;
+        Vector3.Normalize(forward);
+        forward = 1.0f * forward;
+
+        Debug.DrawRay(pos, forward, Color.red, 0.01f);
+
+        this.seeWall = false;
+        RaycastHit hit;
+        if (Physics.SphereCast(pos, this.transform.localScale.x / 2.0f, forward, out hit, forward.magnitude))
+        {
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                this.seeWall = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!this.alive) return;
+
+        // read DNA
+        float turn = 0;
+        float move = this.dna.GetGene(0);
+
+        if (this.seeWall)
+        {
+            turn = this.dna.GetGene(1);
+        }
+
+        this.transform.Translate(0, 0, move * 0.0001f);
+        this.transform.Rotate(0, turn, 0);
+        this.distanceTravelled = Vector3.Distance(this.startPosition, this.transform.position);
+    }
+}
---
+++ Assets/Scripts/GenerateMaze.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMaze : MonoBehaviour {
+
+	public GameObject blockPrefab;
+	int width = 40;
+	int depth = 40;
+
+	// Use this for initialization
+	void Awake () {
+		for(int w = 0; w < width; w++)
+		{
+			for(int d = 0; d < depth; d++)
+			{
+				if( w == 0 || d == 0)	//outside walls bottom and left
+				{
+					GameObject wall = Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y, d + this.transform.position.z), Quaternion.identity);
+                    wall.transform.SetParent(this.transform);
+				}	
+				else if( w < 3 && d < 3)
+				{
+					continue;
+				}		
+				else if(w == width-1 || d == depth-1) //outside walls top and right
+				{
+                    GameObject wall = Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+                    wall.transform.SetParent(this.transform);
+                }
+				else if(Random.Range(0,5) < 1)
+				{
+                    GameObject wall = Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+                    wall.transform.SetParent(this.transform);
+                }
+			}	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
